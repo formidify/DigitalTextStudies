fb0ce61e4fce2416e7ef4db32772b0786ca56523
Jinhui Yang <James@vpn-sgateway-40-56.dyn.carleton.edu>
2017-12-26 17:20:47 +0800

    Added speech tag for ngrams

--- ngramsNLTK.py
+++ ngramsNLTK.py
@@ -7,15 +7,29 @@ def main():
 		reader = csv.DictReader(file, delimiter = ',')
 		corpus = ''
 		fdist = []
+
 		for row in reader:
 			line = row['status_message']
 			for ch in '!@#$%^&*()_+-=;:",./<>?\\':
 				line = line.replace(ch, ' ')
+
+
 			tokens = nltk.word_tokenize(line)
+
+			data = []
+			for token in tokens:
+				data = data + nltk.pos_tag(nltk.word_tokenize(token))
+
+			for i in range(len(data)):
+				if 'CD' in data[i][1]:
+					tokens[i] = 'num'
+
 			ngs = nltk.ngrams(tokens, 4)
 			fdist += ngs
+
 		fdist = nltk.FreqDist(fdist)
 		fdist = fdist.most_common(50)
+		
 		for i in range(50):
 			word= fdist[i][0]
 			count = fdist[i][1]
